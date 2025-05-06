b4ab7ca639eb964bea6c04555ae2cede05ff3682
luisitcho <luishenrisc1@gmail.com>
2025-05-06 00:42:39 -0300

    curso vue - renderização de lista

--- 1-conceitos-iniciais/js/main.js
+++ 1-conceitos-iniciais/js/main.js
@@ -1,12 +1,23 @@
-const { createApp, ref } = Vue
+const { createApp, ref } = Vue;
 
 createApp({
-  setup() {
-    return {
-      image: ref('./assets/img/t-shirt-blue.png'),
-      img_product: 'Camiseta Azul',
-      product_title: 'T-Shirt',
-      in_stock: ref(0),
-    }
-  }
-}).mount('#app')
+    setup() {
+        return {
+            image: ref("./assets/img/t-shirt-blue.png"),
+            img_product: "Camiseta Azul",
+            product_title: "T-Shirt",
+            in_stock: ref(0),
+            details: ["50% Cotton", "30% Polyester", "20% Wool"],
+            variants: [
+                {
+                    id: 0,
+                    color: "blue",
+                },
+                {
+                    id: 1,
+                    color: "green",
+                },
+            ],
+        };
+    },
+}).mount("#app");
