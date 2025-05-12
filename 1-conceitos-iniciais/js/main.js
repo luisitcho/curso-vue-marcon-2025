08c74fd56d7f3cc10d51221d2267d3cc5a7dc7df
luisitcho <luishenrisc1@gmail.com>
2025-05-11 22:54:24 -0300

    curso vue - eventos manuais

--- 1-conceitos-iniciais/js/main.js
+++ 1-conceitos-iniciais/js/main.js
@@ -2,8 +2,18 @@ const { createApp, ref } = Vue;
 
 createApp({
     setup() {
+        const cart = ref(0);
+        function addToCart() {
+            this.cart += 1;
+        }
+
+        const image = ref("./assets/img/t-shirt-blue.png");
+        function changeImage(variant) {
+            image.value = variant;
+        }
+
         return {
-            image: ref("./assets/img/t-shirt-blue.png"),
+            image,
             img_product: "Camiseta Azul",
             product_title: "T-Shirt",
             in_stock: ref(0),
@@ -12,12 +22,17 @@ createApp({
                 {
                     id: 0,
                     color: "blue",
+                    image: "./assets/img/t-shirt-blue.png",
                 },
                 {
                     id: 1,
                     color: "green",
+                    image: "./assets/img/t-shirt-green.png",
                 },
             ],
+            cart,
+            addToCart,
+            changeImage,
         };
     },
 }).mount("#app");
