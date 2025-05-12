1a9c1c14655e8cc47fa4903454392b8bb8001a73
luisitcho <luishenrisc1@gmail.com>
2025-05-11 23:33:49 -0300

    curso vue - propriedade computada

--- 1-conceitos-iniciais/js/main.js
+++ 1-conceitos-iniciais/js/main.js
@@ -1,10 +1,10 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue;
 
 createApp({
     setup() {
         const cart = ref(0);
         function addToCart() {
-            this.cart += 1;
+            cart.value += 1;
         }
 
         const image = ref("./assets/img/t-shirt-blue.png");
@@ -12,11 +12,28 @@ createApp({
             image.value = variant;
         }
 
+        const product_title = "T-Shirt";
+        const brand = "Vue Mastery";
+        const title = computed(() => {
+            return `${product_title} ${brand}`;
+        });
+
+        const in_stock = 100;
+        const inStock = computed(() => {
+            const rules = [
+                { condition: (qty) => qty > 20, message: "In Stock" },
+                { condition: (qty) => qty > 1, message: "Almost out" },
+                { condition: () => true, message: "Out of Stock" },
+            ];
+
+            return rules.find((rule) => rule.condition(in_stock.value)).message;
+        });
+
+        const isOutOfStock = computed(() => in_stock < 1);
+
         return {
             image,
             img_product: "Camiseta Azul",
-            product_title: "T-Shirt",
-            in_stock: ref(0),
             details: ["50% Cotton", "30% Polyester", "20% Wool"],
             variants: [
                 {
@@ -33,6 +50,9 @@ createApp({
             cart,
             addToCart,
             changeImage,
+            title,
+            inStock,
+            isOutOfStock
         };
     },
 }).mount("#app");
